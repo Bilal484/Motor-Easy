75b9dacba7b868152561b2ec88378d482027a7b3
Bilal484 <abilalbro007@gmail.com>
2024-05-27 19:24:05 +0500

    bilal | Update

--- assets/js/about_us.js
+++ assets/js/about_us.js
@@ -33,14 +33,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // ////////////// Scroll Nav bar ///////////////////
-window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) { // Adjust the scroll position as needed
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
+// window.addEventListener('scroll', function () {
+//     const navbar = document.querySelector('.navbar');
+//     if (window.scrollY > 50) { // Adjust the scroll position as needed
+//         navbar.classList.add('scrolled');
+//     } else {
+//         navbar.classList.remove('scrolled');
+//     }
+// });
 
 // ////////// Counter
 
